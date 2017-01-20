84a04c8c1fc4ce9e1bd22dd82a26f8ddb9569694
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-01-19 23:36:43 -0500

    Remove old MeshLine

--- development/scripts/models/map-feature_road.js
+++ development/scripts/models/map-feature_road.js
@@ -4,8 +4,7 @@ const THREE = require("three");
 const MapFeature = require("./map-feature");
 const consts = require("../consts");
 
-const MeshLine = require("../THREE.MeshLine").MeshLine;
-const MeshLineMaterial = require("../THREE.MeshLine").MeshLineMaterial;
+const {MeshLine, MeshLineMaterial} = require("three.meshline");
 
 module.exports = MapFeature.extend({
 	props: {
@@ -21,24 +20,24 @@ module.exports = MapFeature.extend({
 	getMesh: function() {
 
 		const self = this;
-		
+
 		const materials = {};
-		
+
 		console.time(this.name);
-		
+
 		this.points = this.points
 		.map(feature => {
-			
+
 			let expandedFeature = [];
-			
+
 			if (feature.geometry.type === "MultiLineString") {
 				expandedFeature = feature.geometry.coordinates.map(coords => {
-					
+
 					const newFeature = {
 						properties: {},
 						geometry: {}
 					};
-					
+
 					newFeature.properties = feature.properties;
 					newFeature.geometry.type = "LineString";
 					newFeature.geometry.coordinates = coords;
@@ -47,10 +46,10 @@ module.exports = MapFeature.extend({
 			} else {
 				expandedFeature.push(feature);
 			}
-			
+
 			return expandedFeature;
 		}).reduce((a, b) => a.concat(b));
-				
+
 		const lines = this.points
 		.filter(feature => {
 			return [
@@ -67,10 +66,10 @@ module.exports = MapFeature.extend({
 			].indexOf(feature.properties.type) !== -1;
 		})
 		.map(function(feature) {
-			
+
 			let width = 1;
 			let color = consts.COLOR_ROADS_MINOR;
-			
+
 			if (feature.properties.type === "motorway") {
 				width = 3;
 				color = consts.COLOR_ROADS_MAJOR;
@@ -79,17 +78,17 @@ module.exports = MapFeature.extend({
 			} else if (feature.properties.type === "secondary") {
 				width = 2;
 			}
-			
+
 			if (["bus", "sc", "max", "at", "subway", "rt"].indexOf(feature.properties.type) !== -1) {
 				color = consts.COLOR_ROADS_TRANSIT;
 			}
-			
+
 			function getMaterial() {
 				const tunnel = feature.properties.tunnel || false;
 				const materialKey = width.toString() + color.toString() + tunnel.toString();
-				
+
 				let material = materials[materialKey];
-				
+
 				if (!material) {
 					material = new MeshLineMaterial({
 						lineWidth: 10 * width,//size of individual street
@@ -100,17 +99,17 @@ module.exports = MapFeature.extend({
 						color: new THREE.Color(color),
 						blending: tunnel ? THREE.AdditiveAlphaBlending : THREE.NormalBlending
 					});
-					
+
 					materials[materialKey] = material;
 				}
-				
+
 				return material;
 			}
-			
+
 			const coords = feature.geometry.coordinates;
-						
+
 			const lineGeometry = new Float32Array(coords.length * 3);
-			
+
 			coords.forEach((point, index) => {
 				lineGeometry[index * 3 + 0] = point[0];
 				lineGeometry[index * 3 + 1] = point[1];
@@ -119,17 +118,17 @@ module.exports = MapFeature.extend({
 
 			const line = new MeshLine();
 			line.setGeometry(lineGeometry);
-			
+
 			const mesh = new THREE.Mesh(line.geometry, getMaterial());
 			mesh.renderOrder = self.renderOrder;
 			mesh.position.z = self.z_position;
 			mesh.matrixAutoUpdate = false;
-			
+
 			return mesh;
 		});
-		
+
 		const plane = new THREE.Object3D();
-		
+
 		lines.forEach(line => plane.add(line));
 		plane.renderOrder = this.renderOrder;
 		plane.userData.hide_at_z = self.hide_at_z;
