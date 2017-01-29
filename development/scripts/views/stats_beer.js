00a4e62e1f4f9523bdc79e44451d4628b4691fe9
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-01-28 23:05:20 -0500

    add chart to beer page

--- development/scripts/views/stats_beer.js
+++ development/scripts/views/stats_beer.js
@@ -1,8 +1,11 @@
-const View = require("ampersand-view");
+"use strict";
 
-module.exports = View.extend({
+const StatsView = require("./stats");
+const xhr = require("xhr");
+
+module.exports = StatsView.extend({
 	template: `
-		<section>
+		<section class="beer">
 			<header>
 				<div class="art"></div>
 				<div class="copy">
@@ -21,13 +24,18 @@ module.exports = View.extend({
 					</div>
 					<div class="stat">
 						<h3>Est. Volume (L)</h3>
-						<h2>332.11</h2><!--383.058431372549 * 867-->
+						<h2>332.11</h2><!--383.058431372549mL * 867-->
 					</div>
-					<div class="graph full">
 
+					<div class="graph-holder full">
+						<div class="divider"></div>
+						<h3>Beers by Week</h3>
+						<svg></svg>
+						<div class="divider"></div>
 					</div>
+
 					<div class="stat">
-						<h3>Breweries</h3>
+						<h3>Breweries enjoyed</h3>
 						<h2>202</h2>
 					</div>
 					<div class="stat">
@@ -36,7 +44,7 @@ module.exports = View.extend({
 					</div>
 					<div class="stat">
 						<h3>Longest dry spell (days)</h3>
-						<h2>4</h2>
+						<h2>3</h2>
 					</div>
 				</div>
 
@@ -68,5 +76,25 @@ module.exports = View.extend({
 				</div>
 			</main>
 		</section>
-	`
+	`,
+	render() {
+		this.renderWithTemplate();
+
+		const graphContainer = this.query(".graph-holder");
+
+		xhr({
+			method: "GET",
+			uri: "/data/weekly-beer-data.json",
+			json: true
+		}, (err, response, data) => {
+
+			if (err) {
+				return console.error(err);
+			}
+
+			requestAnimationFrame(() => this.buildChart(graphContainer, data));
+		});
+
+		return this;
+	}
 });
