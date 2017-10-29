48cdaf4a7efd7fb35f8b6e29c8372ae71d0ca72c
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2017-10-29 16:11:11 -0400

    Update deps; Remove custom simple-statistics definition file; Remove jshint + jscs files
    Update types for node, ampersand-model, ampersand-state, simple-statistics, tslint; Update autoprefixer, browserify, eslint, jshint, typescript-eslint-parser

--- data-scripts/src/simple-statistics.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "simple-statistics" {
-	export function mean(x: number[]): number;
-	export function median(x: number[]): number;
-	export function max(x: number[]): number;
-	export function sum(x: number[]): number;
-	export function modeFast(x: any[]);
-}
