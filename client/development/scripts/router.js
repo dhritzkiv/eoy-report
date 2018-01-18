d655e1d3c171f3c3425ead9928f20663e5e3b0b4
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-01-18 01:28:24 -0500

    Add places page

--- client/development/scripts/router.js
+++ client/development/scripts/router.js
@@ -31,7 +31,8 @@ const Router = AmpersandRouter.extend({
 		"media": "media",
 		"beer": "beer",
 		"health": "health",
-		"maps": "maps"
+		"maps": "maps",
+		"places": "places"
 	},
 	start: function() {
 
@@ -1473,6 +1474,249 @@ const Router = AmpersandRouter.extend({
 					]
 				},
 				tall: "y2"
+			}
+		]);
+
+		this.trigger("newPage", view);
+		this.trigger("navigation");
+	},
+	places() {
+		const view = new StatsPage({
+			name: "places"
+		});
+
+		view.stats.add([
+			{
+				title: "Total visits",
+				data: {
+					type: "numeric",
+					value: 1202
+				}
+			},
+			{
+				title: "Unique places",
+				data: {
+					type: "numeric",
+					value: 550
+				}
+			},
+			{
+				title: "Average visits per day of week",
+				data: {
+					type: "bar",
+					value: [
+						{label: "Mon", value: 3},
+						{label: "Tue", value: 3.5},
+						{label: "Wed", value: 3.67},
+						{label: "Thr", value: 3.37},
+						{label: "Fri", value: 3.5},
+						{label: "Sat", value: 3.69},
+						{label: "Sun", value: 2.34}
+					]
+				},
+				wide: "x1"
+			},
+			{
+				title: "Longest no-visit streak (days)",
+				data: {
+					type: "numeric",
+					value: 2
+				}
+			},
+			{
+				title: "Longest visit streak (days)",
+				data: {
+					type: "numeric",
+					value: 114
+				}
+			},
+			{
+				title: "Longest visit streak (visits)",
+				data: {
+					type: "numeric",
+					value: 428
+				}
+			},
+			{
+				title: "Visits by month",
+				data: {
+					type: "bar",
+					value: [
+						{label: "Jan", value: 94},
+						{label: "Feb", value: 164},
+						{label: "Mar", value: 93},
+						{label: "Apr", value: 96},
+						{label: "May", value: 102},
+						{label: "Jun", value: 96},
+						{label: "Jul", value: 115},
+						{label: "Aug", value: 103},
+						{label: "Sep", value: 108},
+						{label: "Oct", value: 86},
+						{label: "Nov", value: 77},
+						{label: "Dec", value: 68}
+					]
+				},
+				wide: "x2"
+			},
+			{
+				title: "Top countries (visits)",
+				data: {
+					type: "percentage",
+					value: [
+						["Canada", 1033],
+						["Australia", 100],
+						["New Zealand", 61],
+						["China", 5],
+						["United States of America", 3]
+					]
+				},
+				tall: "y2"
+			},
+			{
+				title: "Top countries (places)",
+				data: {
+					type: "percentage",
+					value: [
+						["Canada", 377],
+						["Australia", 87],
+						["New Zealand", 56],
+						["China", 4],
+						["United States of America", 3]
+					]
+				},
+				tall: "y2"
+			},
+			{
+				title: "Top cities (visits)",
+				data: {
+					type: "percentage",
+					value: [
+						["Toronto", 955],
+						["Sydney ", 55],
+						["Auckland", 48],
+						["Melbourne", 29],
+						["Montreal", 13],
+						["Mississauga", 10],
+						["Vancouver", 9],
+						["Oakville", 8],
+						["Richmond, BC", 4],
+						["Oro-Medonte", 4],
+						["Hamilton, ON", 4],
+						["Katoomba", 4],
+						["Oneroa", 3],
+						["Hamilton, NZ", 3],
+						["Shanghai", 5],
+						["Barrie", 2],
+						["Squamish", 2],
+						["Surrey, BC", 2],
+						["Newmarket, ON", 2],
+						["Ottawa", 2],
+						["Bellingham", 2],
+						["Hahei", 2]
+					]
+				},
+				tall: "y2"
+			},
+			{
+				title: "Top cities (places)",
+				data: {
+					type: "percentage",
+					value: [
+						["Toronto", 316],
+						["Sydney", 47],
+						["Auckland", 41],
+						["Melbourne", 27],
+						["Montreal", 13],
+						["Mississauga", 9],
+						["Vancouver", 8],
+						["Katoomba", 4],
+						["Oakville", 4],
+						["Hamilton, ON", 3],
+						["Richmond, BC", 3],
+						["Newmarket, NZ", 3],
+						["Hamilton, NZ", 3],
+						["Oneroa", 3],
+						["Shanghai", 2],
+						["Hahei", 2],
+						["Ottawa", 2],
+						["Squamish", 2],
+						["Bellingham", 2],
+						["Surrey, BC", 2],
+						["Barrie", 2],
+						["Oro-Medonte", 2]
+					]
+				},
+				tall: "y2"
+			},
+			{
+				title: "Top categories (visits)",
+				data: {
+					type: "percentage",
+					value: [
+						["Café / Coffee Shop", 234],
+						["Liquor Store", 71],
+						["Bar / Pub", 144],
+						["Misc. Restaurant", 80],
+						["Brewery", 66],
+						["Mexican", 48],
+						["Park / Green Space", 32],
+						["Bubble Tea Shop", 28],
+						["Burger Joint", 25],
+						["Cinema", 27],
+						["Vietnamese Restaurant", 19],
+						["Fried Chicken Joint", 15],
+						["Burrito Place", 15],
+						["Bakery", 14],
+						["Airport", 13],
+						["Barbershop", 12],
+						["Kombucha Shop", 10],
+						["Doner Restaurant", 12],
+						["Dessert Shop", 12],
+						["Butcher", 10],
+						["Smoothie Shop", 10],
+						["Sushi Restaurant", 9],
+						["Ice Cream Shop", 9],
+						["Tapas Restaurant", 8],
+						["Bagel Shop", 8],
+						["Furniture Store", 8],
+						["Bike Shop", 8],
+						["Beach", 8],
+						["Juice Bar", 7],
+						["Dim Sum Restaurant", 7],
+						["Asian Restaurant", 7],
+						["Concert Hall", 5],
+						["Pizza Place", 5],
+						["Donut Shop", 5],
+						["Deli", 5],
+						["Poutine Place", 5]
+					]
+				},
+				tall: "y2"
+			},
+			{
+				title: "Top burger places (visits)",
+				data: {
+					type: "percentage",
+					value: [
+						["The Burgernator", 5],
+						["The Burger's Priest (Fashion District)", 4],
+						["BQM Burger (Ossington)", 2],
+						["A&W (College & Ossington)", 2]
+					]
+				},
+				tall: "y2"
+			},
+			{
+				title: "Top Mexican places (visits)",
+				data: {
+					type: "percentage",
+					value: [
+						["Seven Lives Tacos Y Mariscos", 23],
+						["La Chilaca Taquira", 15],
+						["Torteria San Cosme", 5]
+					]
+				},
+				tall: "y1"
 			}
 		]);
 
