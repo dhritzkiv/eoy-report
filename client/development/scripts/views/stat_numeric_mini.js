4dc377e0dba1a386f4f3b8ce080a3ccff0cb4963
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-01-31 23:25:51 -0500

    Basic numeric stat text fitting code

--- client/development/scripts/views/stat_numeric_mini.js
+++ client/development/scripts/views/stat_numeric_mini.js
@@ -7,6 +7,8 @@ const bindings = {
 	}
 };
 
+const asyncAnimationFrame = () => new Promise(resolve => requestAnimationFrame(resolve));
+
 const NumericStatMiniView = StatView.extend({
 	template: `
 		<article class="stat">
@@ -14,11 +16,61 @@ const NumericStatMiniView = StatView.extend({
 				<h2 data-hook="title"></h2>
 			</header>
 			<main>
-				<h3 data-hook="value"></h3>
+				<h3><span data-hook="value"></span></h3>
 			</main>
 		</article>
 	`,
-	bindings
+	bindings,
+	/**
+	 *
+	 * @param {HTMLSpanElement} spanEl
+	 */
+	async sizeTextToFit(spanEl) {
+		const parentEl = spanEl.parentElement;
+		let fontSizeEm = 1.0;
+		let letterSpacingEm = 0;
+		let affectingLetterSpacing = true;
+
+		//reset styles
+		spanEl.style.letterSpacing = `${letterSpacingEm}em`;
+		spanEl.style.fontSize = `${fontSizeEm}em`;
+
+		await asyncAnimationFrame();
+
+		while (parentEl.clientWidth < spanEl.clientWidth) {
+
+			if (affectingLetterSpacing && letterSpacingEm > -0.1) {
+				for (let i = 0; i < 10; i++) {
+					letterSpacingEm -= 0.0015;
+					spanEl.style.letterSpacing = `${letterSpacingEm}em`;
+
+					if (parentEl.clientWidth < spanEl.clientWidth) {
+						break;
+					}
+				}
+			} else {
+				fontSizeEm -= 0.0125;
+				spanEl.style.fontSize = `${fontSizeEm}em`;
+			}
+
+			affectingLetterSpacing = !affectingLetterSpacing;
+		}
+
+	},
+	render() {
+		this.renderWithTemplate();
+
+		const valueSpan = this.queryByHook("value");
+		const sizeTextToFitBound = this.sizeTextToFit.bind(this, valueSpan);
+
+		sizeTextToFitBound();
+
+		window.addEventListener("resize", sizeTextToFitBound);
+
+		this.once("remove", () => window.removeEventListener("resize", sizeTextToFitBound));
+
+		return this;
+	}
 });
 
 export default NumericStatMiniView;
