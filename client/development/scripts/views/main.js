ab515d32a4321566ac58841b4944a6329a7267b0
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-02-24 22:00:50 -0500

    Shimmer icons on load

--- client/development/scripts/views/main.js
+++ client/development/scripts/views/main.js
@@ -13,6 +13,9 @@ import svg_health from "../../img/category-icon_health.svg";
 import svg_media from "../../img/category-icon_media.svg";
 import svg_walking from "../../img/category-icon_walking.svg";
 
+const asyncRAF = () => new Promise(resolve => requestAnimationFrame(resolve));
+const asyncSetTimemout = (delay) => new Promise(resolve => setTimeout(resolve, delay));
+
 const MainView = View.extend({
 	template: (`
 	<body>
@@ -65,7 +68,7 @@ const MainView = View.extend({
 
 		this.modeSwitcher.set(view);
 	},*/
-	render: function() {
+	render() {
 		this.renderWithTemplate();
 
 		const pageContainer = this.pageContainer = this.query(".page-container");
@@ -98,6 +101,8 @@ const MainView = View.extend({
 			});
 		});
 
+		this.shimmerIcons();
+
 		return this;
 	},
 	linkClick: function(event) {
@@ -109,6 +114,65 @@ const MainView = View.extend({
 
 		app.router.navigate(`${pathname}${search}`);
 		event.preventDefault();
+	},
+	async shimmerIcons() {
+		await asyncRAF();
+		await asyncSetTimemout(1250);
+
+		/** @type {HTMLElement[]} */
+		let iconEls = this.queryAll(".category-icon");
+		/** @type {HTMLElement} */
+		const iconsHolder = this.query(".categories-list");
+		const iconsHolderRect = iconsHolder.getBoundingClientRect();
+
+		const normalizeAngle = (value, start, end) => {
+			const width = end - start;
+			const offsetValue = value - start;
+
+			return (offsetValue - (Math.floor(offsetValue / width) * width)) + start;
+		};
+
+		const rad2Deg = rad => rad * (180 / Math.PI);
+
+		const iconsCenter = {
+			x: (iconsHolderRect.left + iconsHolderRect.right) / 2,
+			y: (iconsHolderRect.top + iconsHolderRect.bottom) / 2
+		};
+
+		let topLeftTheta;
+
+		const sortingGroups = iconEls
+		.map(icon => ({rect: icon.getBoundingClientRect(), icon}))
+		.map(({rect, icon}, index) => {
+			const center = {
+				x: (rect.left + rect.right) / 2,
+				y: (rect.top + rect.bottom) / 2
+			};
+
+			let theta = rad2Deg(Math.atan2(center.y - iconsCenter.y, center.x - iconsCenter.x));
+
+			if (index === 0) {
+				topLeftTheta = normalizeAngle(theta, 0, 360);
+			}
+
+			theta -= topLeftTheta;
+			theta = normalizeAngle(theta, 0, 360);
+
+			return {
+				theta,
+				icon
+			};
+		})
+		.sort(({theta: a}, {theta: b}) => a - b);
+
+		iconEls = sortingGroups.map(({icon}) => icon);
+
+		for (const iconEl of iconEls) {
+			iconEl.classList.add("active");
+			await asyncSetTimemout(125);
+
+			setTimeout(() => iconEl.classList.remove("active"), 600);
+		}
 	}
 });
 
